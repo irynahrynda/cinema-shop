7867e4ff20cf4c8d5b893c587c777d400297c880
Iryna Hrynda <hryndairyna1@gmail.com>
2022-06-16 15:06:43 +0300

    - added spring to the project

---
+++ src/main/java/cinema/dto/response/ShoppingCartResponseDto.java
@@ -0,0 +1,10 @@
+package cinema.dto.response;
+
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class ShoppingCartResponseDto {
+    private Long userId;
+    private List<Long> ticketIds;
+}
